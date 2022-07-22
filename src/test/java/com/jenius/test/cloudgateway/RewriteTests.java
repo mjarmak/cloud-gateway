46f594aa9ef67ee8c7d01a5727a44a6aa1c98e73
mjarmak <mohamadjarmak@gmail.com>
2022-07-22 15:18:50 +0200

    added tests

---
+++ src/test/java/com/jenius/test/cloudgateway/RewriteTests.java
@@ -0,0 +1,105 @@
+package com.jenius.test.cloudgateway;
+
+import com.sun.net.httpserver.HttpServer;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class RewriteTests {
+
+    private static final Logger log = LoggerFactory.getLogger(RewriteTests.class);
+
+    // Create a running HttpServer that echoes back the request URL.
+    private static HttpServer startTestServer() {
+
+        try {
+            log.info("[I26] Starting mock server");
+            HttpServer mockServer = HttpServer.create();
+            mockServer.bind(new InetSocketAddress(0), 0);
+            mockServer.createContext("/api", (xchg) -> {
+                String uri = xchg.getRequestURI()
+                        .toString();
+                log.info("[I23] Backend called: uri={}", uri);
+                xchg.getResponseHeaders()
+                        .add("Content-Type", "text/plain");
+                xchg.sendResponseHeaders(200, 0);
+                OutputStream os = xchg.getResponseBody();
+                os.write(uri.getBytes());
+                os.flush();
+                os.close();
+            });
+
+            mockServer.start();
+            InetSocketAddress localAddr = mockServer.getAddress();
+            log.info("[I36] mock server started: local address={}", localAddr);
+
+            return mockServer;
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+
+    }
+
+    @LocalServerPort
+    private int localPort;
+
+    @DynamicPropertySource
+    static void registerBackendServer(DynamicPropertyRegistry registry) {
+        registry.add("rewrite.backend.uri", () -> {
+            HttpServer s = startTestServer();
+            return "http://localhost:" + s.getAddress().getPort();
+        });
+    }
+
+    @Test
+    void test1(@Autowired WebTestClient webClient) {
+        webClient.get()
+                .uri("http://localhost:" + localPort + "?red=green")
+                .exchange()
+                .expectBody()
+                .consumeWith((result) -> {
+                    String body = new String(Objects.requireNonNull(result.getResponseBody()));
+                    assertTrue(body.contains("google.com/?red=green"));
+                });
+    }
+
+    @Test
+    void test2(@Autowired WebTestClient webClient) {
+        webClient.get()
+                .uri("http://localhost:" + localPort + "")
+                .exchange()
+                .expectBody()
+                .consumeWith((result) -> {
+                    String body = new String(Objects.requireNonNull(result.getResponseBody()));
+                    assertTrue(body.contains("Example Domain"));
+                });
+    }
+
+    @Test
+    void test3(@Autowired WebTestClient webClient) {
+        webClient.get()
+                .uri("http://localhost:" + localPort + "/baeldung/spring-cloud-gateway")
+                .exchange()
+                .expectBody()
+                .consumeWith((result) -> {
+                    String body = new String(Objects.requireNonNull(result.getResponseBody()));
+                    assertTrue(body.contains("Spring Cloud Gateway"));
+                });
+    }
+
+}
